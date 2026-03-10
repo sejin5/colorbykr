e5f90c8cafe68296aeedcede3836555f494ef364
sejin5 <sejin5.dev@gmail.com>
2026-03-10 23:49:25 +0900

    assets: icons 추가 및 svgr 적용

---
+++ src/components/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as BgLight } from "./BgLight";
+export { default as Dark } from "./Dark";
+export { default as Letter } from "./Letter";
+export { default as Light } from "./Light";
+export { default as Menu } from "./Menu";
+export { default as Palette } from "./Palette";
+export { default as Pause } from "./Pause";
+export { default as Play } from "./Play";
