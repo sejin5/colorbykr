22e487b88da6270115e29c5af489938e48351215
sejin5 <sejin5.dev@gmail.com>
2026-05-06 20:50:08 +0900

    feat: queryclient 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import Cards from "./components/cards";
 import { BgColorProvider } from "./context/bgColorContext";
 import { ModalProvider } from "./context/modalContext";
-import Modal from "./components/modal";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./pages/Home";
+import Color from "./pages/ColorPage";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <BgColorProvider>
-        <ModalProvider>
-          <h1>작업중</h1>
-          <Cards />
-          <Modal />
-        </ModalProvider>
-      </BgColorProvider>
+      <QueryClientProvider client={queryClient}>
+        <BgColorProvider>
+          <ModalProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/colors/:code" element={<Color />} />
+              </Routes>
+            </BrowserRouter>
+          </ModalProvider>
+        </BgColorProvider>
+      </QueryClientProvider>
     </>
   );
 }
