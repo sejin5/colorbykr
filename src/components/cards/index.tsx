71c835cb23012de3c936f6d4e8b2bf016dad00bb
sejin5 <sejin5.dev@gmail.com>
2026-05-06 20:50:32 +0900

    refactor: type 분리 및 mock데이터 제거

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,16 +1,20 @@
 import styled from "styled-components";
 import "../../Temp.css";
 import Row from "./row";
-import { colorArr } from "../../mock";
 import { chunkArray } from "../../utils/chunkArray";
+import type { Colors } from "../../constants/color";
 
 const StyledDiv = styled.div`
   width: 100%;
   overflow: hidden;
 `;
 
-const Cards = () => {
-  const rows = chunkArray(colorArr, 3);
+interface Props {
+  colors: Colors[];
+}
+
+const Cards = ({ colors }: Props) => {
+  const rows = chunkArray(colors, 3);
 
   return (
     <StyledDiv className="scroll-container">
